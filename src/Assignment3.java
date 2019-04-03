44f331f015d7918d9ad5dbf5dec29b700828fd8d
Christopher Devito <cdevito2@uwo.ca>
2019-04-03 12:59:15 -0400

    program shell added

---
+++ src/Assignment3.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Flow {
+
+//last two arguments indicate the
+//labels/numbers assigned to the vertices (start and end nodes
+
+    public void maximizeFlowNetwork(Graph fN, int s, int t)
+    {
+
+    }
+
+
+    public int breadthFirstPathSearch(Graph FN, int s, int d)
+    {
+
+    }
+
+
+
+
+}
