9dc11ef03f6fe6c29331e5e013361bfe88edd699
= <=>
2019-04-08 02:37:27 -0400

    implementing maximizeFlowNetwork(...)

--- src/Assignment3.java
+++ src/Assignment3.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
 
-//cdevito2 //mdsilva6
+//cdevito2, mdsilva6
 class Flow {
 
     // made interfaces for Graph and Edge to remove errors
@@ -23,43 +23,56 @@ class Flow {
 
     public void maximizeFlowNetwork(Graph fN, int s, int t)
     {
-        while(breadthFirstPathSearch(fN, s, t)==1){
+        int parent[] = new int[fn.numVerticies()];
+        int maxFlow = 0;
+        while(breadthFirstPathSearch(fN, s, t)==1){ // runs while these is a path, as determined by BFS algorithm
+            int pathFlow = Integer.MAX_VALUE; // this is cuase to calculate the bottleneck, so it is set to a very high value
 
+            for( int i = 0; i!= s; i = parent[i]){
+                int j = parent[i];
+                pathFlow = Math.min(pathFlow, fN.opposite(j,i)) // returns the minimum of the two values
+            }
+
+            for (int i = t; i != s; i = parent[i]){
+                int j = parent[i];
+                fN.opposite(j,i) -= pathFlow; // accounting for backflow 
+                fN.opposite(i,j) += pathFlow;
+
+            }
+            maxFlow += pathFlow;
         }
-
     }
 
 
     public int breadthFirstPathSearch(Graph FN, int s, int d)
     {
-        int visitedNodes[] = new int[FN.numVertices()];
-        int parent[] = new int[FN.numVerticies()]; // one less?
+        int visitedNodes[] = new int[FN.numVertices()]; // array to store visited vertices
+        int parent[] = new int[FN.numVerticies()];
         //initializes an array that is equal in size to the number of vertices in the graph
         for (int i = 0; i < FN.numVertices(); i ++){
             visitedNodes[i] = 0; //setting all elements in visitedNodes array to 0
         }
-        LinkedList<Integer> queue  = new LinkedList<Integer>() { // change to Queue?
-        };
-        queue.add(s); //change to enqueue(s)?
+        LinkedListQueue<Integer> queue  = new LinkedListQueue<Integer>; // Declaring a queue to store the nodes
+        queue.enqueue(s);
         visitedNodes[s] = 1;
         parent[s] = -1;
 
-        while(!queue.isEmpty()){
+        while(!queue.isEmpty()){ // runs while the queue is not empty
             int x = queue.poll();
-            for (int j = 0; j < FN.numVertices(); j++){
-                Edge<Integer> edge = FN.getEdge(s,j);
+            for (int j = 0; j < FN.numVertices(); j++){ // checking edges
                 if(visitedNodes[j] == 0 && (edge.flowCapacity() - edge.flow())>0){
+                    // if this case is true, then there is an edge
                     parent[j] = x;
-                    queue.add(j);
+                    queue.enqueue(j);
                     visitedNodes[j] = 1;
                 }
             }
         }
         if (visitedNodes[d] == 1){
-            return 1;
+            return 1; // return 1 if destination node has been visited
         }
         else
-            return 0;
+            return 0; // else returns 0
 
     }
 
